9c731f069fd33126cdd67e51344bdb0b3cdaa709
shivanijatav26 <shivanijatavshivi@gmail.com>
2025-12-05 14:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function addStudent() {
+    // Table के tbody को select करें
+    let table = document.getElementById("studentTable").getElementsByTagName('tbody')[0];
+    
+    // New row create करें
+    let newRow = table.insertRow();
+    
+    // Cells create करें
+    let nameCell = newRow.insertCell(0);
+    let ageCell = newRow.insertCell(1);
+    let branchCell = newRow.insertCell(2);
+    
+    // Data set करें
+    nameCell.innerHTML = "Rahul";
+    ageCell.innerHTML = "22";
+    branchCell.innerHTML = "CSE";
+}
